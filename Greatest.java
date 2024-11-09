5d170a1adebcec78e9b58f12aba91d4603c2e2c8
EC2 Default User <ec2-user@ip-172-31-39-143.eu-north-1.compute.internal>
2024-11-09 09:43:25 +0000

    message

---
+++ Greatest.java
@@ -0,0 +1,22 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.lang.*;
+import java.util.Scanner;
+class Greatest
+
+{
+ public static void main(String[] args)
+    {
+     int num =0;
+     if (num % 2 == 0)
+     {
+       System.out.println("Entered Number is Even");
+     }
+ 
+        else
+     {
+      System.out.println("Entered Number is Odd");
+     }
+    }
+}
